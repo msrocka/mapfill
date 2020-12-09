a006444bbab45f3d5fc162b22dd7f4f4527f87a0
msrocka <michael.srocka@gmail.com>
2020-12-09 13:01:52 +0100

    parse arguments

--- README.md
+++ README.md
@@ -4,3 +4,26 @@ files](https://github.com/GreenDelta/olca-modules/blob/master/doc/flow_mapping_c
 It takes a CSV mapping file and a data source, adds the meta-data of the flows
 to the input or output side of the mapping file, and writes the updated mappings
 back to the file.
+
+## Usage
+
+```
+The mapfill command takes the following arguments:
+
+mapfill -map [m] -on [o] -from [f]
+
+with:
+
+* m : the path to the CSV mapping file
+* o : indicates which flows of the mapping should be updated:
+      'source' or 's': for the flows on the source side
+      'target' or 't': for the flows on the target side
+* f : the path of the file that contains the meta-data, this can be
+      * an EcoSpold 1 zip file
+      * an EcoSpold 2 zip file
+      * an ILCD package
+      * an openLCA database directory
+      * a SimaPro CSV file
+
+`mapfill -h` or `mapfill help` will print this help
+```
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,74 @@
 import java.io.File
 
+class Config {
+    var mappingFile = ""
+    var side = ""
+    var source = ""
+}
+
+enum class Side {
+    Source,
+    Target
+}
+
 fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        printHelp()
+        return
+    }
+
+    // parse arguments
+    var flag: String? = null
+    val config = Config()
+    for (arg in args) {
+        if (arg == "-h" || arg == "help") {
+            printHelp()
+            return
+        }
+        if (arg.startsWith("-")) {
+            flag = arg
+            continue
+        }
+        if (flag == null) {
+            println(
+                "ERROR: invalid arguments; " +
+                        "use `help` for more information"
+            )
+            return
+        }
+        when (flag) {
+            "-map" -> config.mappingFile = arg
+            "-on" -> config.side = arg
+            "-from" -> config.source = arg
+        }
+    }
+
+
     val ilcd = "C:/Users/Win10/Projects/databases/_dumps/EF-v3.0.zip"
     ILCDCollector(File(ilcd)).collect()
+}
+
+private fun printHelp() {
+    println(
+        """
+        The mapfill command takes the following arguments:
+
+        mapfill -map [m] -on [o] -from [f]
+
+        with:
+
+        * m : the path to the CSV mapping file
+        * o : indicates which flows of the mapping should be updated:
+              'source' or 's': for the flows on the source side
+              'target' or 't': for the flows on the target side
+        * f : the path of the file that contains the meta-data, this can be
+              * an EcoSpold 1 zip file
+              * an EcoSpold 2 zip file
+              * an ILCD package
+              * an openLCA database directory
+              * a SimaPro CSV file
+
+        `mapfill -h` or `mapfill help` will print this help
+    """.trimIndent()
+    )
 }
